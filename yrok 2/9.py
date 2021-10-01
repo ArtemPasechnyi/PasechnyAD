6f02910d93eab7e441b2a692d174b9ca035f3e98
ArtemPasechnyi <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-10-01 12:00:31 +0300

    Delete 9.py

--- yrok 2/9.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-n = int(input('Введите первое число - '))
-m = int(input('Введите второе число - '))
-k = int(input('Введите количество долек - '))
-if (n * m > k and (k % m ==0 or k % n == 0)):
-    print('Да')
-else:
-    print('Нет')
