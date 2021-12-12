ed08f51273e33a9911a215f432517c6c571ef8d7
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-12-12 17:05:19 +0300

    Update 3.py

--- yrok 4/3.py
+++ yrok 4/3.py
@@ -1,8 +1,8 @@
 # -- coding: utf-8 --
-def cut_string():
-    S = str(input('Введите строку: '))
-    l = len(S) // 2
-    S_first = (S[-l:])
-    S_second = (S[:(len(S) - l)])
-    print(S_first + S_second)
-cut_string()
+def cut():
+    s = str(input('Введите строчку: '))
+    l = len(s) // 2
+    s_1 = (s[-l:])
+    S_2 = (s[:(len(s) - l)])
+    print(s_1 + s_2)
+cut()
