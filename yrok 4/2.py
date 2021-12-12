87527c86b4af65baa83d81a8856971a325f22619
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-12-12 16:59:10 +0300

    Update 2.py

--- yrok 4/2.py
+++ yrok 4/2.py
@@ -1,6 +1,6 @@
 # -- coding: utf-8 --
-def count_words():
-    S = str(input('Введите строку: '))
-    n = S.count(' ')
-    print(n + 1)
-count_words()
+def count():
+    s = str(input('Введите строчку: '))
+    k = s.count(' ')
+    print(k + 1)
+count()
