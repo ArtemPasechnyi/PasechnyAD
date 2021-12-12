ac5f0d13ed5cb0ba729c70cf2e188c4634ac3533
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-12-12 17:48:37 +0300

    Update 9.py

--- yrok 4/9.py
+++ yrok 4/9.py
@@ -1,6 +1,6 @@
 # -- coding: utf-8 --
-def delete():
-    S = str(input('Введите строку: '))
-    symbol = str(input('Введите символ для удаления: '))
-    print(S.replace(symbol, ''))
-delete()
+def d():
+    s = str(input('Введите строчку: '))
+    simvol = str(input('Введите символ для удаления: '))
+    print(s.replace(simvol, ''))
+d()
