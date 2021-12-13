addd9d34ac92bf276829cad1556088fb6310e9c7
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-12-13 17:25:47 +0300

    Update 5.py

--- yrok 4/5.py
+++ yrok 4/5.py
@@ -1,9 +1,9 @@
 # -- coding: utf-8 --
 def cf():
-    s = input("ввести строчку - ")
+    s = input("Введите строчку: ")
     if (s.count("f") == 1):
         print(s.find("f"))
     if (s.count("f") >= 2):
-        print("первый индекс - ", s.find("f"))
-        print("последний индекс - ", s.rfind("f"))
+        print("первый индекc: ", s.find("f"))
+        print("последний индекс: ", s.rfind("f"))
 cf()
