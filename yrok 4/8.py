2f600a984fb222b7dcd6bbe9adfd28050f90f43e
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2021-12-13 17:20:20 +0300

    Update 8.py

--- yrok 4/8.py
+++ yrok 4/8.py
@@ -3,6 +3,6 @@ def inv():
     s = str(input('Введите строчку: '))
     h_1 = s.find('h')
     h_2 = s.rfind('h')
-    s = s[h_1 + 1:h_2]
-    print(s[::-1])
+    print(s[:h_1 + 1] + s[h_1 + 1:h_2][::-1] + s[h_2:])
+    
 inv()
