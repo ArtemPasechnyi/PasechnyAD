a163c33cbaeece2a5af8e544a9e988a21e085b6b
coca8732 <91153498+ArtemPasechnyi@users.noreply.github.com>
2022-03-30 23:26:10 +0300

    Add files via upload

---
+++ java1/Main3.java
@@ -0,0 +1,16 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (String arg : args) {
+            System.out.println(arg + " ");
+            int k = Integer.parseInt(arg);
+            sum += k;
+        }
+        if (args.length == 2) {
+            System.out.println(args[0]+ " + " + args[1] + " = " + sum);
+        }
+        else {
+            System.out.println("Неверное количество параметров");
+        }
+    }
+}
---
+++ java1/Main4.java
@@ -0,0 +1,13 @@
+public class Main4 {
+    public static void main(String[] args) {
+        String password = "w311";
+        String login = "admin";
+
+        if ((password.equals(args[1]) && login.equals(args[0])) || ((password.equals(args[0]) && login.equals(args[1])))) {
+            System.out.println("Вас узнали. Добро пожаловать");
+        } else {
+            System.out.println("Логин и пароль не распознаны. Доступ запрещён");
+        }
+
+    }
+}
